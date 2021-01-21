bd8eb3e72af85aa03c6a632ea4396bbe5d4b6462
Jenna Lambert <57016635+TeslaPrince@users.noreply.github.com>
2021-01-21 01:33:55 -0600

    Add files via upload

---
+++ Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim water, initial, final, energy As Double
+        Const eq As Integer = 4184
+        water = CDbl(txtWater.Text)
+        initial = CDbl(txtInitial.Text)
+        final = CDbl(txtFinal.Text)
+        energy = CDbl(water * (final - initial) * eq)
+        txtEnergy.Text = String.Format("{0:N2}", energy)
+    End Sub
+End Class
